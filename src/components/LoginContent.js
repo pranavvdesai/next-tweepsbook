426e90df7e22a94ea8600cd807946613adce9f5b
pranavvdesai <pranavdesai.psd@gmail.com>
2022-10-13 02:39:01 +0530

    fix: grammar

--- src/components/LoginContent.js
+++ src/components/LoginContent.js
@@ -140,14 +140,14 @@ const LoginContent = (props) => {
                     disabled
                     class="cursor-not-allowed inline-block ml-2 px-7 py-3 bg-orange-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    Send
+                    Verify
                   </button>
                 ) : (
                   <button
                     type="submit"
                     class="inline-block ml-2 px-7 py-3 bg-orange-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    Send
+                    Verify
                   </button>
                 )}
               </div>
